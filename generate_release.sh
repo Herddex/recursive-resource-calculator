4936aa1e1ccb68524d9e3abdcd407d26b30fea52
914-Jinga-Tudor <jingatudor474@gmail.com>
2021-10-18 12:13:27 +0300

    1.0.1v3

---
+++ generate_release.sh
@@ -0,0 +1,11 @@
+$dir_name=RecursiveResourceCalculator_$1
+mkdir $dir_name
+cp gui $dir_name/gui
+cp locale $dir_name/locale
+cp logic $dir_name/logic
+cp changelog.txt $dir_name/changelog.txt
+cp control.lua $dir_name/control.lua
+cp data.lua $dir_name/data.lua
+cp info.json $dir_name/info.json
+cp LICENSE $dir_name/LICENSE
+cp thumbnail.png $dir_name/thumbnail.png
