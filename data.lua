33c0802df7abd26afe6c8b518ef2525dfa836ca3
Herddex <jingatudor474@gmail.com>
2025-08-03 18:42:25 +0300

    1.1.7

--- data.lua
+++ data.lua
@@ -3,6 +3,6 @@ data:extend({
         type = "custom-input",
         name = "hxrrc_toggle_calculator",
         localised_name = {"hxrrc.toggle_calculator"},
-        key_sequence = "ALT + C",
+        key_sequence = "ALT + X",
     },
 })
